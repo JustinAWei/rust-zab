1af05dcf964dc462d79ff84b191dd3b7a8f39b65
Justin Wei <wei.justin.a@gmail.com>
2020-04-20 13:14:06 -0500

    set up basic structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
+use std::thread;
+use std::collections::HashMap;
+
 fn main() {
+    // create the node objects
+    // start them up
+    // wait until we terminate the program
     println!("Hello, world!");
+}
+
+// node
+struct Node {
+    id: i64,
+    leader: bool,
+    value: i64,
+    tx: HashMap<i64, Sender<Message> >,
+    rx: HashMap<i64, Receiver<Message> >
+}
+
+struct Message {
+    val: i64
 }
