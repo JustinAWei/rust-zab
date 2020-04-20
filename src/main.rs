ca8fd9398a9d5b462214af77a09762cef637e9ba
Justin Wei <wei.justin.a@gmail.com>
2020-04-20 13:29:05 -0500

    added main loop for node

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,40 @@ struct Node {
     rx: HashMap<i64, Receiver<Message> >
 }
 
+impl Node {
+    fn send(&self, id:i64, msg: Message) {
+        self.tx[&id].send(msg).unwrap();
+    }
+    fn receive(&self, id: i64) -> Message {
+        self.rx[&id].recv().unwrap()
+    }
+
+    fn process(&self, msg:Message) {
+        let val = msg.val;
+        println!("{}", val);
+    }
+
+    fn operate(&mut self) {
+        if self.id == 0 {
+            // leader
+            self.value += 1;
+            for id in 1..5 {
+                self.send(id, Message {val: self.value});
+            }
+            for id in 1..5 {
+                let ack = self.receive(id);
+                assert!(ack.val == -1);
+            }
+        } else {
+            // follower
+            for i in 0..3 {
+                let msg = self.receive(0);
+                self.process(msg);
+            }
+        }
+    }
+}
+
 struct Message {
     val: i64
 }
