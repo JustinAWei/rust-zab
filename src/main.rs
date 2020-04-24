34e062225bbaa954e13817684181ee39321be372
Antony Yun <antony.n.yun@gmail.com>
2020-04-23 19:28:01 -0500

    Refactor message types and start implementing leader

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use std::sync::mpsc::{Sender, Receiver, channel};
 use std::thread;
-use std::collections::HashMap;
+use std::collections::{HashSet, HashMap};
 
 fn main() {
     // create the node objects
     let mut nodes = Vec::new();
     let rnodes = &nodes;
-    let N = 5;
+    let N: usize = 5;
 
     // leader
-    nodes.push(Node::new(0, true));
+    nodes.push(Node::new(0, N, true));
 
     // followers
     for i in 1..N {
-        nodes.push(Node::new(i, false));
+        nodes.push(Node::new(i, N, false));
     }
 
     // register tx channels for each node
@@ -33,7 +33,7 @@ fn main() {
     // start them up
     for mut node in nodes {
         let handle = thread::spawn(move || {
-            node.operate();
+            node.main_loop();
         });
         handles.push(handle);
     }
@@ -46,23 +46,46 @@ fn main() {
     println!("Hello, world!");
 }
 
+struct InflightTxn {
+    data: String,
+    ack_ids: HashSet<usize>,
+}
+
 // node
 struct Node {
-    id: i64,
+    id: usize,
+    cluster_size: usize,
+    quorum_size: usize,
     leader: bool,
-    value: i64,
-    tx: HashMap<i64, Sender<Message> >,
+    committed_zxid: i64,
+    latest_zxid: i64,
+    tx: HashMap<usize, Sender<Message>>,
     sx: Sender<Message>,
     rx: Receiver<Message>,
+    txn_log: Vec<(i64, String)>,
+    inflight_txns: HashMap<i64, InflightTxn>,
 }
 
 impl Node {
-    fn new(i: i64, isLeader: bool) -> Node {
+    fn new(i: usize, cluster_size: usize, is_leader: bool) -> Node {
+        assert!(cluster_size % 2 == 1);
         let (s, r) = channel();
-        Node {id: i, leader: isLeader, value: 0, tx: HashMap::new(), sx: s, rx: r}
+        Node {
+            id: i,
+            cluster_size: cluster_size,
+            quorum_size: (cluster_size + 1) / 2,
+            leader: is_leader,
+            committed_zxid: 0,
+            latest_zxid: 0,
+            tx: HashMap::new(),
+            sx: s,
+            rx: r,
+            txn_log: Vec::new(),
+            inflight_txns: HashMap::new(),
+        }
     }
 
-    fn register(&mut self, id: i64, tx: Sender<Message>) {
+    fn register(&mut self, id: usize, tx: Sender<Message>) {
         match self.tx.insert(id, tx) {
             Some(v) => {
                 println!("Error in register! value already present {:?}", v);
@@ -71,45 +94,84 @@ impl Node {
         };
     }
 
-    fn send(&self, id:i64, msg: Message) {
-        println!("node {} sending {} to {}", self.id, msg.val, id);
+    fn send(&self, id: usize, msg: Message) {
+        println!("node {} sending {:?} to {}", self.id, msg, id);
         self.tx[&id].send(msg).unwrap();
         //println!("send successful");
     }
     fn receive(&self) -> Message {
         //println!("node {} receiving...", self.id);
         let m = self.rx.recv().unwrap();
-        println!("node {} received {}", self.id, m.val);
+        println!("node {} received {:?}", self.id, m);
         m
     }
 
-    fn process(&mut self, msg:Message) {
-        self.value = msg.val;
+    fn record_txn(&mut self, zxid: i64, data: String) {
+        self.txn_log.push((zxid, data));
     }
 
-    fn operate(&mut self) {
+    fn process(&mut self, msg:Message) {
         if self.leader {
-            // leader
-            self.value += 1;
-            for id in 1..5 {
-                self.send(id, Message {val: self.value});
-            }
-            for _ in 1..5 {
-                let ack = self.receive();
-                assert!(ack.val == self.value);
-            }
-            println!("leader done!");
+            self.process_leader(msg);
         } else {
-            // follower
+            self.process_follower(msg);
+        }
+    }
+
+    fn process_leader(&mut self, msg: Message) {
+        match msg.msg_type {
+            MessageType::ClientProposal(data) => {
+                self.latest_zxid += 1;
+                let txn = InflightTxn {
+                    data: data.clone(),
+                    ack_ids: HashSet::new(),
+                };
+                self.inflight_txns.insert(self.latest_zxid, txn);
+                let send_msg = Message {
+                    sender_id: self.id,
+                    msg_type: MessageType::Proposal(self.latest_zxid, data),
+                };
+                for id in 1..self.cluster_size {
+                    self.send(id, send_msg.clone());
+                }
+                // TODO: spawn timeout
+            },
+            MessageType::Ack(zxid) => {
+                match self.inflight_txns.get_mut(&zxid) {
+                    Some(t) => {
+                        t.ack_ids.insert(msg.sender_id);
+                        if t.ack_ids.len() >= self.quorum_size {
+                            // TODO handle quorum
+                            println!("quorum!");
+                        }
+                    },
+                    None => {},
+                };
+            },
+            _ => {
+                println!("Unsupported msg type for leader");
+            }
+        };
+    }
+
+    fn process_follower(&mut self, msg: Message) {
+        // TODO this
+    }
+
+    fn main_loop(&mut self) {
+        while true {
             let msg = self.receive();
             self.process(msg);
-            self.send(0, Message {val: self.value});
-            println!("follower done!");
         }
+        /*
+        */
     }
 }
 
+#[derive(Clone, Debug)]
 enum MessageType {
+    // Zab message types
+    /*
     FollowerInfo(i64, String),
     Diff(i64),
     Trunc(i64),
@@ -119,12 +181,18 @@ enum MessageType {
     AckEpoch(i64),
     NewLeader(i64),
     UpToDate,
-    Proposal(i64),
+    */
+    Proposal(i64, String),
     Ack(i64),
     Commit(i64),
-    Inform(i64),
+    // Inform(i64),
+    //
+    // Our message types
+    ClientProposal(String),
 }
 
+#[derive(Clone, Debug)]
 struct Message {
-    val: i64,
+    sender_id: usize,
+    msg_type: MessageType,
 }
